beb040e43da50949bf541cfca670eb21b7d0f6c6
harukakawamoto <86188830+harukakawamoto@users.noreply.github.com>
2022-06-27 16:09:31 +0900

    Add files via upload

--- overlay.py
+++ overlay.py
@@ -32,13 +32,21 @@ class CvOverlayImage(object):
         overlay_height, overlay_width = cv_overlay_image.shape[:2]
 
         # OpenCV形式の画像をPIL形式に変換(α値含む)
+
         # 背景画像
+        #PILで画像を扱うためにRGBからBGRに変換する
         cv_rgb_bg_image = cv.cvtColor(cv_background_image, cv.COLOR_BGR2RGB)
+        #変換したPILをarrayに変換
         pil_rgb_bg_image = Image.fromarray(cv_rgb_bg_image)
+        #RGBAに変換
         pil_rgba_bg_image = pil_rgb_bg_image.convert('RGBA')
+
         # オーバーレイ画像
+        #PILで画像を扱うためにRGBからBGRに変換する
         cv_rgb_ol_image = cv.cvtColor(cv_overlay_image, cv.COLOR_BGRA2RGBA)
+        #変換したPILをarrayに変換
         pil_rgb_ol_image = Image.fromarray(cv_rgb_ol_image)
+        #RGBAに変換
         pil_rgba_ol_image = pil_rgb_ol_image.convert('RGBA')
 
         # composite()は同サイズ画像同士が必須のため、合成用画像を用意
